b8a08706c87d48ff47df856741b2f8353a65b941
vsakun <vsakun@gmail.com>
2017-07-30 22:17:36 +0300

    JS&JQ

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/*Прилипание блока к верхнему краю экрана 
+при прокручивании до него, а так же описание поведения верхнего меню*/
 $(document).ready(function () {
   var objToStick = $("nav"); //Получаем нужный объект
   var topOfObjToStick = $(objToStick).offset().top; //Получаем начальное расположение нашего блока
@@ -36,7 +38,7 @@ $(document).ready(function () {
     };
   });
 });
-
+/*действия кнопок в главном меню*/
 $(function(){
 	$('.smal_mn').on('click',function(){
 		$('.glob_mn').removeClass("glob_fix_add");
@@ -53,7 +55,7 @@ $(function(){
 		$('.smal_mn p').addClass("p_fix");
 	});
 });
-
+/*Видоизменения меню в зависимости он ширины(+ чистка ненужных классов)*/
 function windowSize(){
 	if($(window).width() > '768'){
 		if($("all_fix")){
@@ -71,7 +73,7 @@ else if($(window).width() < '768'){
 };
 $(window).on('load resize',windowSize);
 
-
+/*Главный слайдер*/
 function plusSlides(n){
 	var m1 = $(".dot[name=first]");
 	var m2 = $(".dot[name=double]");
@@ -101,8 +103,6 @@ function plusSlides(n){
 		};
 	}
 }
-
-
 function currentSlide(n){
 	if(n == 1 || n == 0){
 		$(".sl_cntr").removeClass("act_c");
@@ -138,3 +138,31 @@ function currentSlide(n){
 		$(".prev").attr({"onclick":"plusSlides(-1)"});
 	}
 }
+
+/*Функция POPUP. На блок который надо увеличивать необходимо установить name="box"*/
+function popup(){
+	$("[name = 'box']").on('click', function(){
+		var copy = $(this).clone();
+		console.log($(this).parent().append('<div class="popup"></div>'));
+		$(copy).appendTo(".popup");
+		$(".popup").append('<i class="fa fa-times"></i>');
+		$(document).mouseup(function (e){ // событие клика по веб-документу
+			var div = $("[name = 'box']");
+			var cl = $(".popup i[class=fa-times]"); // тут указываем ID элемента
+			if (!div.is(e.target) && div.has(e.target).length === 0){ // и не по его дочерним элементам
+				$("div.popup").remove(); // скрываем его
+			}
+			if(cl.is(e.target) && cl.has(e.target).length ===0){
+				$("div.popup").remove();
+			}
+		});
+	});
+	$(document).bind('keydown', function(eventObject){
+		if(eventObject.keyCode == 27){
+			$("div.popup").remove();
+		}
+	});
+}
+
+
+popup();
